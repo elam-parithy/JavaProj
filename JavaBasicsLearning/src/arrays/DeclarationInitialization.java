8b9976fa9206712775a55924c8a156ddee9b03c0
Elas <Elas@Elas-PC>
2023-08-20 13:50:36 +0530

    Initialization and declaration statements

---
+++ JavaBasicsLearning/src/arrays/DeclarationInitialization.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class DeclarationInitialization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1[] = new int[3];
+		
+		
+
+	}
+
+}
